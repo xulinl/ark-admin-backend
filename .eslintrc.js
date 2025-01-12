eefe4d63ee2c37e7a105ac0a53d97d85211eba20
xulin <1834033926@qq.com>
2025-01-12 16:43:02 +0800

    fix: 修改eslint报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+/*
+ * @Description:
+ * @FilePath: \ark-admin-backend\.eslintrc.js
+ */
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,5 +25,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
